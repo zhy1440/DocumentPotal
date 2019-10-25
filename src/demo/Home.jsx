a1d94bd3868e02521329148080d41ff1fab03c8d
cathy <cathy@Windows10_DEV>
2019-10-25 06:10:30 -0400

    aaa

--- src/demo/Home.jsx
+++ src/demo/Home.jsx
@@ -4,7 +4,6 @@ import ButtonAppBar from "./ButtonAppBar";
 class Home extends Component {
     render() {
         return <div>home</div>;
-        // return <ButtonAppBar/>;
     }
 }
 export default Home;
